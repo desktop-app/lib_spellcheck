640706f4088ad16ea8515f21e9c0c8e6b487635a
23rd <23rd@vivaldi.net>
2019-07-27 13:59:33 +0300

    Added new initial platform files.

---
+++ spellcheck/platform/platform_spellcheck.h
@@ -0,0 +1,21 @@
+// This file is part of Desktop App Toolkit,
+// a set of libraries for developing nice desktop applications.
+//
+// For license and copyright information please follow this link:
+// https://github.com/desktop-app/legal/blob/master/LEGAL
+//
+#pragma once
+
+namespace Platform {
+
+[[nodiscard]] bool CheckSpelling(const QString wordToCheck, int tag);
+
+} // namespace Platform
+
+#ifdef Q_OS_MAC
+#include "spellcheck/platform/mac/spellcheck_mac.h"
+#elif defined Q_OS_LINUX // Q_OS_MAC
+#include "spellcheck/platform/linux/spellcheck_linux.h"
+#elif defined Q_OS_WIN // Q_OS_MAC || Q_OS_LINUX
+#include "spellcheck/platform/win/spellcheck_win.h"
+#endif // Q_OS_MAC || Q_OS_LINUX || Q_OS_WIN
