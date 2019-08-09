4698543899ce19ea150d5d42411ac38b7d600e04
23rd <23rd@vivaldi.net>
2019-08-09 20:47:07 +0300

    Added assistant class for spellchecker.
     - It helps to recognize that word belongs to certain script.

---
+++ spellcheck/chat_helpers/spellchecker_helper.h
@@ -0,0 +1,12 @@
+// This file is part of Desktop App Toolkit,
+// a set of libraries for developing nice desktop applications.
+//
+// For license and copyright information please follow this link:
+// https://github.com/desktop-app/legal/blob/master/LEGAL
+//
+
+namespace SpellCheckerHelper {
+
+bool IsWordSkippable(const QStringRef &word);
+
+} // namespace SpellCheckerHelper
