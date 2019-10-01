83a49f9b58a0eb65a77ad417fd2e1c27514dfd06
23rd <23rd@vivaldi.net>
2019-10-01 21:32:07 +0300

    Added checking whole text in background for Windows.

--- spellcheck/platform/win/spellcheck_win.cpp
+++ spellcheck/platform/win/spellcheck_win.cpp
@@ -46,6 +46,9 @@ public:
 	void fillSuggestionList(
 		const QString &wrongWord,
 		std::vector<QString> *optionalSuggestions);
+	void checkSpellingText(
+		const QString &text,
+		MisspelledWords *misspelledWordRanges);
 
 private:
 
@@ -151,6 +154,69 @@ bool WindowsSpellChecker::checkSpelling(const QString& word) {
 	return false;
 }
 
+void WindowsSpellChecker::checkSpellingText(
+	const QString &text,
+	MisspelledWords *misspelledWordRanges) {
+
+	// The spellchecker marks words not from its own language as misspelled.
+	// So we only return words that are marked
+	// as misspelled in all spellcheckers.
+	MisspelledWords misspelledWords;
+
+	constexpr auto isActionGood = [](auto action) {
+		return action == CORRECTIVE_ACTION_GET_SUGGESTIONS
+			|| action == CORRECTIVE_ACTION_REPLACE;
+	};
+
+	for (const auto &[_, spellchecker] : _spellcheckerMap) {
+		ComPtr<IEnumSpellingError> spellingErrors;
+
+		HRESULT hr = spellchecker->ComprehensiveCheck(
+			Q2WString(text),
+			&spellingErrors);
+		if (!(SUCCEEDED(hr) && spellingErrors)) {
+			continue;
+		}
+
+		MisspelledWords tempMisspelled;
+		ComPtr<ISpellingError> spellingError;
+		for (; hr == S_OK; hr = spellingErrors->Next(&spellingError)) {
+			ULONG startIndex = 0;
+			ULONG errorLength = 0;
+			CORRECTIVE_ACTION action = CORRECTIVE_ACTION_NONE;
+
+			if (!(SUCCEEDED(hr)
+				&& spellingError
+				&& SUCCEEDED(spellingError->get_StartIndex(&startIndex))
+				&& SUCCEEDED(spellingError->get_Length(&errorLength))
+				&& SUCCEEDED(spellingError->get_CorrectiveAction(&action))
+				&& isActionGood(action))) {
+				continue;
+			}
+			const auto word = std::make_pair(
+				(int) startIndex,
+				(int) errorLength);
+			if (misspelledWords.empty()
+				|| ranges::find_if(misspelledWords, [&](auto &range) {
+					return (word == range);
+				}) != misspelledWords.end()) {
+				tempMisspelled.push_back(std::move(word));
+			}
+		}
+		// If the tempMisspelled vector is empty at least once,
+		// it means that the all words will be correct in the end
+		// and it makes no sense to check other languages.
+		if (tempMisspelled.empty()) {
+			return;
+		}
+		misspelledWords = std::move(tempMisspelled);
+	}
+	misspelledWordRanges->insert(
+		misspelledWordRanges->end(),
+		misspelledWords.begin(),
+		misspelledWords.end());
+}
+
 void WindowsSpellChecker::addWord(const QString& word) {
 	for (const auto &[_, spellchecker] : _spellcheckerMap) {
 		spellchecker->Add(Q2WString(word));
@@ -219,6 +285,12 @@ void IgnoreWord(const QString &word) {
 bool IsWordInDictionary(const QString &wordToCheck) {
 	// ISpellChecker can't check if a word is in the dictionary.
 	return false;
+}
+
+void CheckSpellingText(
+	const QString &text,
+	MisspelledWords *misspelledWordRanges) {
+	SharedSpellChecker()->checkSpellingText(text, misspelledWordRanges);
 }
 
 
